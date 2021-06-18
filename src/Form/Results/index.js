c1461bc4d334ede798695cad29f4666c3cff388b
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-18 02:11:22 +0200

    formatting

--- src/Form/Results/index.js
+++ src/Form/Results/index.js
@@ -1,8 +1,10 @@
-const Results = ({ result }) => (
-     !!result && (
-        <p className="form__results">
-            {result.amount} {result.currencyFrom} to {result.value.toFixed(2)} {result.currencyTo}</p>
-    )
-);
-
+const Results = ({ result }) => {
+          
+    return (
+        !!result && (
+            <p className="form__results">
+                {result.amount} {result.currencyFrom} to {result.value.toFixed(2)} {result.currencyTo}</p>
+        )
+    );
+}
 export default Results;
