37b8962e719cf383e592fdd8eba5e85f00ca7fbe
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-21 19:22:37 +0200

    formating

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
 
-const Container = ({children}) => (
-<div className="container">
-    {children}
-</div>
+const Container = ({ children }) => (
+    <div className="container">
+        {children}
+    </div>
 );
 
 export default Container;
