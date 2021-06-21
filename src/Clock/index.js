cec9f9a66cdf1ae30709f9ca090594375008d3a0
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-22 00:29:46 +0200

    formatDate func created

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,17 +1,19 @@
 
 import "./style.css";
 
+const formatDate = (date) => date.toLocaleDateString(undefined, {
+    month: "long",
+    weekday: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+});
+
 const Clock = ({ date }) => (
     <div className="clock">
-        {date.toLocaleDateString(undefined, {
-            month: "long",
-            weekday: "long",
-            day: "numeric",
-            year: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric"
-        })}
+        {formatDate(date)}
     </div>
 );
 
