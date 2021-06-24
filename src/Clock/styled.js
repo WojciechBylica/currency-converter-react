1fb3f8b23777de881048d82f6c5a41cc89879185
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-24 22:54:56 +0200

    style.css content moved to styled.js

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -1,6 +1,8 @@
-.clock {
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
     display: flex;
     justify-content: flex-end;
     font-size: 12px;
     font-family: monospace;
-}
+`;
