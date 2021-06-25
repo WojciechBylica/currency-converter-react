c8f12f8ba724baac53a7903875416595f0ba0644
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-25 16:50:36 +0200

    formating

--- src/currencies.js
+++ src/currencies.js
@@ -1,26 +1,24 @@
-
-    const currencies = [
-        {
-          id: "PLN",
-          name: "PLN",
-          value: 1,
-        },
-        {
-          id: "EUR",
-          name: "EUR",
-          value: 4.45,
-        },
-        {
-          id: "USD",
-          name: "USD",
-          value: 4.2,
-        },
-        {
-          id: "RUB",
-          name: "RUB",
-          value: 0.2,
-        },
-      ];
-
+const currencies = [
+  {
+    id: "PLN",
+    name: "PLN",
+    value: 1,
+  },
+  {
+    id: "EUR",
+    name: "EUR",
+    value: 4.45,
+  },
+  {
+    id: "USD",
+    name: "USD",
+    value: 4.2,
+  },
+  {
+    id: "RUB",
+    name: "RUB",
+    value: 0.2,
+  },
+];
 
 export default currencies;
