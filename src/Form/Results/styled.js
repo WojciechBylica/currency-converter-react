7e07e8d1778a2eca44f303e6d43b6a0e6d8e27ac
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-25 16:51:38 +0200

    theme added (border, border-radius, bg-color)

--- src/Form/Results/styled.js
+++ src/Form/Results/styled.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const Result = styled.p`
     text-align: center;
     width: 100%;
-    border: 1px solid grey;
-    border-radius: 5px;
+    border: ${({ theme }) => theme.ElementBorder};
+    border-radius: ${({ theme }) => theme.ElementBorderRadius};
     padding: 12px;
     font-size: 16px;
     margin: 0px 2px;
-    background-color: white;
+    background-color: ${({ theme }) => theme.BackgroundColor.ElementBackgroundColorSecondary};
     word-break: break-word;
 `;
