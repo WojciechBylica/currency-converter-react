fcccc0deab21c8738cbcf42798e369b3565b68a3
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-25 21:24:59 +0200

    import style.css canceled

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme"
+import { GlobalStyle } from "./GlobalStyle"
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
