82e392f42923edddc83cc1f2b07fd74471e860af
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-03 20:34:45 +0200

    added useDataFromAPI and render conditions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
 import Container from "./Container";
 import Header from "./Header";
 import Main from "./Main";
 import Clock from "./Clock";
 import Form from "./Form";
 import Footer from "./Footer";
+import Loader from "./Loader";
+import Error from "./Error";
+import { useDataFromAPI } from "./useDataFromAPI";
 
 function App() {
+  const {
+    dataFromAPI,
+  } = useDataFromAPI()
+
   return (
     <>
       <Container>
         <Header title="Przelicznik walut" />
         <Main>
           <Clock />
-          <Form />
+          {
+            dataFromAPI.status === "loading" &&
+            <Loader />
+          }
+          {
+            dataFromAPI.status === "success" &&
+            <Form dataFromAPI={dataFromAPI} />
+          }
+          {
+            dataFromAPI.status === "error" &&
+            <Error />
+          }
         </Main>
         <Footer />
       </Container>
