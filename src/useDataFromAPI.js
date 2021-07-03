309728fa9bd5cdc316cad589534d5905ead5ff94
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-03 20:54:14 +0200

    formating

--- src/useDataFromAPI.js
+++ src/useDataFromAPI.js
@@ -16,7 +16,6 @@ export const useDataFromAPI = () => {
                     rates: response.data.rates,
                     status: "success",
                 });
-
             } catch (error) {
                 setDataFromAPI({ status: "error" });
             }
