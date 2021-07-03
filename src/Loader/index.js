543f775318c65b1e8b1566b0a58b4af158a2afe2
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-03 20:32:17 +0200

    Loader element added

---
+++ src/Loader/index.js
@@ -0,0 +1,11 @@
+import { Fieldset } from "../Form/styled";
+
+const Loader = () => (
+    <>
+        <Fieldset>
+            Ładuję kursy walut z Europejskiego Banku Centralnego...
+        </Fieldset>
+    </>
+);
+
+export default Loader;
