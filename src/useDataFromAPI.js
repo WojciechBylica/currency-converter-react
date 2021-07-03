d2e860103d335aba249a41528a7bf4f7ff51dd43
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-03 20:33:39 +0200

    custom hook created/ getting data from API

---
+++ src/useDataFromAPI.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+export const useDataFromAPI = () => {
+    const [dataFromAPI, setDataFromAPI] = useState({
+        date: null,
+        rates: null,
+        status: "loading",
+    })
+    useEffect(() => {
+        const getDataFromAPI = async () => {
+            try {
+                const response = await axios.get('https://api.exchangerate.host/latest?symbols=PLN,EUR,USD,RUB&base=PLN')
+                setDataFromAPI({
+                    date: response.data.date,
+                    rates: response.data.rates,
+                    status: "success",
+                });
+
+            } catch (error) {
+                setDataFromAPI({ status: "error" });
+            }
+        };
+        setTimeout(getDataFromAPI, 2000);
+    }, []);
+    return {
+        dataFromAPI,
+    };
+};
