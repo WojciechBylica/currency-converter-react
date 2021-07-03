3cb5d9f0592433b924de68a5de83d93cdc3e2323
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-03 20:33:52 +0200

    Error element added

---
+++ src/Error/index.js
@@ -0,0 +1,9 @@
+import { Fieldset } from "../Form/styled";
+
+const Error = () => (
+    <Fieldset>
+        <p>Coś poszło nie tak... sprawdź połączenie z internetem i spróbuj ponownie!</p>
+    </Fieldset>
+);
+
+export default Error;
