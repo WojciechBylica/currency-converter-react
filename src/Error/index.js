ae3ebc3c1bfc6821235553878d15c27a8d355bcc
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-04 03:13:37 +0200

    Error removed

--- src/Error/index.js
+++
@@ -1,8 +0,0 @@
-import { Fieldset } from "../Form/styled";
-
-const Error = () => (
-    <Fieldset>
-        <p>Coś poszło nie tak... sprawdź połączenie z internetem i spróbuj ponownie!</p>
-    </Fieldset>
-);
-export default Error;
