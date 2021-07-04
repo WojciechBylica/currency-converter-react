4c2f36b3b37f0509900374243b3f066824132fb1
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-04 03:13:24 +0200

    Messenger added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Main from "./Main";
 import Clock from "./Clock";
 import Form from "./Form";
 import Footer from "./Footer";
-import Loader from "./Loader";
-import Error from "./Error";
+import Messenger from "./Messenger";
 import { useDataFromAPI } from "./useDataFromAPI";
+import imageLoader from "./images/imageLoader.gif";
+import image from "./images/image.png";
 
 function App() {
   const dataFromAPI = useDataFromAPI()
@@ -15,22 +16,29 @@ function App() {
     <>
       <Container>
         <Header title="Przelicznik walut" />
-        <Main>
-          <Clock />
-          {
-            dataFromAPI.status === "loading" &&
-            <Loader />
-          }
-          {
-            dataFromAPI.status === "success" &&
-            <Form dataFromAPI={dataFromAPI} />
-          }
-          {
-            dataFromAPI.status === "error" &&
-            <Error />
-          }
-        </Main>
-        <Footer />
+        {dataFromAPI.status === "loading" &&
+          <Messenger
+            body="Ładuję kursy walut z Europejskiego Banku Centralnego..."
+            source={imageLoader}
+            alt="imageLoader"
+          />
+        }
+        {dataFromAPI.status === "error" &&
+          <Messenger
+          body="Coś poszło nie tak... Sprawdź połączenie z internetem i spróbuj ponownie!"
+          source={image}
+          alt="image"
+        />
+        }
+        {dataFromAPI.status === "success" &&
+          <>
+            <Main>
+              <Clock />
+              <Form dataFromAPI={dataFromAPI} />
+            </Main>
+            <Footer />
+          </>
+        }
       </Container>
     </>
   );
