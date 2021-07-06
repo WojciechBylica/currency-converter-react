1abdd9e6063ec2a8e388f9ee052ff418ffc06636
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-06 23:20:59 +0200

    not nesecarry Components text moved to APP.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
-import Container from "./Container";
 import Header from "./Header";
 import Main from "./Main";
 import Clock from "./Clock";
 import Form from "./Form";
-import Footer from "./Footer";
 import Messenger from "./Messenger";
 import { useDataFromAPI } from "./useDataFromAPI";
 import imageLoader from "./images/imageLoader.gif";
 import image from "./images/image.png";
+import { StyledDiv, StyledFooter } from "./styled";
 
 function App() {
-  const dataFromNBP = useDataFromAPI()
+  const ratesData = useDataFromAPI()
 
   return (
     <>
-      <Container>
+      <StyledDiv>
         <Header title="Przelicznik walut" />
-        {dataFromNBP.status === "loading" &&
+        {ratesData.status === "loading" &&
           <Messenger
             body="Ładuję kursy walut z Narodowego Banku Polskiego..."
             source={imageLoader}
             alt="imageLoader"
           />
         }
-        {dataFromNBP.status === "error" &&
+        {ratesData.status === "error" &&
           <Messenger
             body="Coś poszło nie tak... Sprawdź połączenie z internetem i spróbuj ponownie!"
             source={image}
             alt="image"
           />
         }
-        {dataFromNBP.status === "success" &&
+        {ratesData.status === "success" &&
           <>
             <Main>
               <Clock />
-              <Form dataFromNBP={dataFromNBP} />
+              <Form ratesData={ratesData} />
             </Main>
-            <Footer />
+            <StyledFooter>Designed by Wojciech Bylica Arts</StyledFooter>
           </>
         }
-      </Container>
+      </StyledDiv>
     </>
   );
 };
