e3842c1d6d24ed37931c711d086d72e475ed005b
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-07 00:12:44 +0200

    func/useStates moved to Form, initialValues deff.

--- src/Form/useForm.js
+++ src/Form/useForm.js
@@ -1,27 +1,21 @@
 import { useState } from "react";
 
 export const useForm = () => {
-    const [hideRates, setHideRates] = useState(true);
-    const toggleHideRates = () => {
-        setHideRates(hideRates => !hideRates);
-    };
     const [amount, setAmount] = useState("");
-    const [currencyFrom, setCurrencyFrom] = useState("0");
-    const [currencyTo, setCurrencyTo] = useState("2");
+    const initialValueFrom = "0";
+    const initialValueTo = "2";
+    const [currencyFrom, setCurrencyFrom] = useState(initialValueFrom);
+    const [currencyTo, setCurrencyTo] = useState(initialValueTo);
     const [result, setResult] = useState();
-    const calculateResult = (amount, valueFrom, valueTo) => +amount * +valueFrom / +valueTo;
 
     return {
         currencyFrom,
         currencyTo,
         setCurrencyFrom,
         setCurrencyTo,
-        calculateResult,
         amount,
         setAmount,
         setResult,
-        hideRates,
-        toggleHideRates,
         result,
     };
 };
