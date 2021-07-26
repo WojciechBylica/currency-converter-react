3b71cd48782883325b84dfb63b806be48db34e3d
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-26 21:02:13 +0200

    styling

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -10,7 +10,6 @@ export const Fieldset = styled.fieldset`
     border-radius: ${({ theme }) => theme.elementBorderRadius};
     padding: 12px;
     font-size: 16px;
-    background-color: ${({ theme }) => theme.backgroundColor.mystic};
     box-shadow: 2px 2px 5px #1d1b1b;
 `;
 
@@ -20,13 +19,47 @@ export const FormField = styled.select`
     padding: 5px;
     border-radius: ${({ theme }) => theme.elementBorderRadius};
     border: ${({ theme }) => theme.elementBorder};
-    background-color: ${({ theme }) => theme.backgroundColor.melrose};
-
-    &:hover {
-        background-color: ${({ theme }) => theme.backgroundColor.melroseHover};
-    }
+    background-color: ${({ theme }) => theme.backgroundColor.white};
+    cursor: pointer;
 `;
 
 export const Input = styled(FormField)`
     background-color: ${({ theme }) => theme.backgroundColor.white};
+    cursor: text;
+`;
+
+export const AnimatedDollar = styled.span`
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 100px;
+    width: 100px;
+    height: 100px;
+    color: #ffd7005e;
+    background: #ffff0030;
+    border: 2px solid #ffd70070;
+    border-radius: 100px;
+    box-shadow: 0 2px 1px #ffd7005e;
+    animation-name: rotationSafari;
+    animation-duration: 5s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-fill-mode: backwards;
+
+    @keyframes rotationSafari {
+        from {
+            color: #ffd70050;
+            transform: rotate3d(0, 1, 0, 0deg);
+        }
+        50% {
+            color: #ffff0050;
+            transform: rotate3d(0, 1, 0, 180deg);
+        }
+        100% {
+            color: #ffd70050;
+            transform: rotate3d(0, 1, 0, 360deg);
+        }
+    }
 `;
